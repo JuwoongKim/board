1900c6b54eea2ae4e0b30d5aad7b8890306002ec
Juwoong <wwong7247@gmail.com>
2023-05-29 22:33:15 +0900

    test: data rest 테스트
    기존에 프레임워크에서 잘 동작하지만 학습을 위해 작성 
    
    data rest 테스트 = 인테그레이션 테스트 -> 디비까지
    나눠서 하기엔 너무 어려웠음 이렇게 밖에 할 수 없었다 --> 추후 개선
    DB 접근이 일어나므로 안전을 위해 `@Transactional` 적용
    테스트 안에서는 롤백 정책으로 동작

---
+++ src/test/java/com/juwoong/board/controller/DataRestTest.java
@@ -0,0 +1,87 @@
+package com.juwoong.board.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+@DisplayName("Data REST - API 테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+//@WebMvcTest   // 단순히  이것 하나만 사용하면 안됨.  동작과정을 통해 안되는 이유 정리, 브라우져 자체에서는 데이터 확인 잘죔
+public class DataRestTest {
+
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+    @DisplayName("[api] 게시글 리스트 조회")
+    @Test
+    void test1() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));  // 왜 vlaue of를 사용해야했는지, hal+json이 무엇인지 
+    }
+
+    @DisplayName("[api] 게시글 단건 조회")
+    @Test
+    void test2() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 게시글 -> 댓글 리스트 조회")
+    @Test
+    void test3() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articles/1/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 리스트 조회")
+    @Test
+    void test4() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 단건 조회")
+    @Test
+    void test5() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articleComments/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+}
