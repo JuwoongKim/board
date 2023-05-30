8400011d53c2a2ee8d1bb24d056c6014e9906271
Juwoong <wwong7247@gmail.com>
2023-05-30 18:13:06 +0900

    feat: 댓글 서비스 로직 구조 및 테스트 코드 작
    댓글과 관련된 단순 crud 기능을 테스트로 표현
    일부만 테스트로 표현 -> 추가 예정

---
+++ src/main/java/com/juwoong/board/service/ArticleCommentService.java
@@ -0,0 +1,28 @@
+package com.juwoong.board.service;
+
+import com.juwoong.board.dto.ArticleCommentDto;
+import com.juwoong.board.repository.ArticleCommentRepository;
+import com.juwoong.board.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+}
